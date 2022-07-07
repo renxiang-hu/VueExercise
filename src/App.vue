3255c75a5726781e7939064fb26f8df8af582e3d
胡仁祥 <18917734365@163.com>
2022-07-07 15:27:51 +0800

    vue项目知识点练习

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>这是App的根组件</h1>
+  <h2>{{username}}</h2>
+  <h3 class="title">这是vue的组件</h3>
+  <button @click="addCount">{{this.count}}</button>
+</template>
+
+<script>
+  export default {
+    name: 'MyApp',
+    data(){
+      return{
+        username: 'hurenxiang',
+        count:0
+      }
+    },
+    methods: {
+      addCount(){
+        this.count++;
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+  h1{
+    color: #f5083b;
+  }
+  .title{
+    color: blue;
+  }
+
+</style>
